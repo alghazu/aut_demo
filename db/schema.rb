d739ac800e4b81279d16165785ec2e6a232b286a
nader <alghazu@gmail.com>
2018-05-06 17:09:10 +0200

    created comments model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_04_122444) do
+ActiveRecord::Schema.define(version: 2018_05_06_150502) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2018_05_04_122444) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "author"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.string "author"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
