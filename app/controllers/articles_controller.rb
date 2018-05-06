938c4b6d1bc571a3c3421e06e217d9801f5f359e
nader <alghazu@gmail.com>
2018-05-06 22:33:05 +0200

    Articles form saves to db

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,14 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    article = Article.create(article_params)
+    if article.persisted?
+      flash[:success] = 'Your article was successfully created'
+      redirect_to root_path
+    else
+      flash[:error] = article.errors.full_messages.first
+      render :new
+    end
   end
 
   def show
