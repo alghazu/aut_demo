699b000490af65f0730e97a13b7aed8b47baaf77
nader <alghazu@gmail.com>
2018-05-07 00:42:54 +0200

    Added comments model, and migration to reference to article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_06_150502) do
+ActiveRecord::Schema.define(version: 2018_05_06_223756) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,10 +22,11 @@ ActiveRecord::Schema.define(version: 2018_05_06_150502) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
-    t.string "author"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "article_id"
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
 end
--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :comment do
     content "MyText"
-    author "MyString"
     email "myemail@example.com"
   end
 end
